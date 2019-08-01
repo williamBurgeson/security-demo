bae809c527a306a89c466c5e0fe5537572c2f83f
William Burgeson <William.Burgeson@googlemail.com>
2019-08-01 20:43:56 +0100

    Added MVC template project with security

---
+++ mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace security_demo_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
