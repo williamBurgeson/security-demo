e475a18d228a2976eff8a3f59a3dec38fd7a23e5
William Burgeson <William.Burgeson@googlemail.com>
2019-08-04 11:14:53 +0100

    WIP

--- angular-6-registration-login-example/src/app/app.module.ts
+++ angular-6-registration-login-example/src/app/app.module.ts
@@ -40,7 +40,7 @@ import { RegisterComponent } from './register';
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
 
         // provider used to create fake backend
-        fakeBackendProvider
+        //fakeBackendProvider
     ],
     bootstrap: [AppComponent]
 })
